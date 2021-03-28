d42c189e7839853d8419062d2c822b19e14f8c1e
ankur-b <barveankur29@outlook.com>
2021-03-28 22:30:27 +0530

    initial commit

---
+++ src/Screens/Details.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+const Details = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Details Screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+export default Details;
